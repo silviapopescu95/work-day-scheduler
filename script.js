2edb94bed0b2fb25a328b88765696165f838f5ed
Silvia Popescu <amazingaivlis@gmail.com>
2020-12-17 13:20:01 -0800

    Created event listener for saving tasks.

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-$(document).ready
+$(document).ready(function(){
+    //onclick for save button
+    $(".btn").on("click", function() {
+        var task = $(this).attr(".form-control").val();
+        var hour = $(this).parent().attr("id");
+        console.log(task);
+        console.log(hour);
+
+        //save in local storage
+        localStorage.setItem(task, hour);
+    });
+});
+
+// Show current date and time
+$("#todaysDate").text(moment().format('dddd MMMM Do YYYY'));
